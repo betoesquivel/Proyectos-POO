1cebddbdd5d7ed5f596a807726ed91ef26cab30c
JosBeto Esquivel <ppbeto94@localhost.(none)>
2013-05-06 22:29:35 -0500

    chromebook...

--- Proyecto Parcial 3/src/proyecto3.cpp
+++ Proyecto Parcial 3/src/proyecto3.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 #include "Reloj.h"
 #include "Registro.h"
@@ -8,9 +8,27 @@ using namespace std;
 #include "TarjetaProveedor.h"
 #include "TarjetaCliente.h"
 
-int main(){
+//información para arreglos de tarjetas
+const int tamMaxTarjetas = 40; 
+char tipoTarjeta[tamMaxTarjetas];
+int idTarjeta[tamMaxTarjetas], idEspecifico[tamMaxTarjetas];
+string tienda-empresa[tamMaxTarjetas];
+//Variable con el índice de una tarjeta en el arreglo
+int indice; 
 
-    cout<<"Hello World! It works!"<<endl;
-    return 0; 
+void cargarTarjetas(string archivo){
+
+	File a;
+	a.open(archivo.c_str());
+	//Loop que lea y cargue las tarjetas en los arreglos paralelos
+	a.close();
+
+}
+
+int main(){
+	
+	Registro estacionados[20];
+	Tarjeta* tarjetas[40];
+	return 0; 
 
 }
